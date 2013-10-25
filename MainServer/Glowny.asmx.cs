fad1f45188b07d8fc9d14d5311f11fd0c3cd8a7d
primu <pstroiwas@gmail.com>
2013-10-25 11:10:07 +0200

    od pawla

---
+++ MainServer/Glowny.asmx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace MainServer
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Glowny : System.Web.Services.WebService
+    {
+        //Tymczasowe deklaracje;
+        static private Komunikat temp = new Komunikat();
+        static private List<Wiadomosc> wiadomosci = new List<Wiadomosc>();
+        static private List<Uzytkownik> uzytkownicy = new List<Uzytkownik>();
+
+        private string connection = "Data Source=(local);"
+               + "Initial Catalog=PokerDB;"
+               + "Persist Security Info=False;"
+               + "User ID=PokerDBUser;Password=zaq478mlp";
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        //Logowanie
+        [WebMethod]
+        public Komunikat Zarejestruj(string nazwa, string haslo, string email)
+        {
+            return temp;
+        }
+        [WebMethod]
+        public Komunikat Zaloguj(string nazwa, string haslo)
+        {
+            return temp;
+        }
+        [WebMethod]
+        public Komunikat ZmienHaslo(string token, string haslo)
+        {
+            return temp;
+        }
+
+        //Chat
+        [WebMethod]
+        public List<Uzytkownik> PobierzUzytkownikow(string token)
+        {
+            return uzytkownicy;
+        }
+
+        [WebMethod]
+        public List<Wiadomosc> PobierzWiadomosci(string token, Int32 timT)
+        {
+            List<Wiadomosc> wiad = new List<Wiadomosc>();
+            wiad.Clear();
+            for (int i = 0; i < wiadomosci.Count; i++)
+            {
+                if (timT < wiadomosci.ElementAt(i).stempelCzasowy)
+                {
+                    wiad.Add(wiadomosci.ElementAt(i));                   
+                }
+            }
+            return wiad;
+        }
+
+        [WebMethod]
+        public Komunikat WyslijWiadomosc(string token, Wiadomosc wiadomosc)
+        {
+            Int32 timer;
+            timer = (Int32)(DateTime.Now.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            wiadomosc.stempelCzasowy = timer;
+            wiadomosci.Add(wiadomosc);
+            temp.trescKomunikatu = "wyslano";
+            return temp;
+        }
+
+        //Serwer-Serwer
+        [WebMethod]
+        public Komunikat ZweryfikujUzytkownika(string token, string nazwa)
+        {
+            return temp;
+        }
+        //co dalej?
+        [WebMethod]
+        public string TestujPolaczenieZBaza()
+        {
+            SqlConnection conn = 
+            new SqlConnection();
+ 
+            conn.Open();
+ 
+            // create a SqlCommand object for this connection
+            SqlCommand command = conn.CreateCommand();
+            command.CommandText = "Select * from Uzytkownik";
+            command.CommandType = CommandType.Text;
+ 
+            // execute the command that returns a SqlDataReader
+            SqlDataReader reader = command.ExecuteReader();
+ 
+            // display the results
+            while (reader.Read()) 
+            {
+                string output = reader["Nazwa"].ToString();
+                //Console.WriteLine(output);
+            }
+ 
+            // close the connection
+            reader.Close();
+            conn.Close();
+            return "OK";
+        }
+
+
+    }
+}
---
+++ MainServer/UkladyKart.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MainServer
+{
+    public class UkladyKart
+    {
+      Karta[] tab = new Karta[52]; 
+      static Karta.figuraKarty[] figury = { Karta.figuraKarty.K2, Karta.figuraKarty.K3, Karta.figuraKarty.K4, Karta.figuraKarty.K5, Karta.figuraKarty.K6, Karta.figuraKarty.K7, Karta.figuraKarty.K8, Karta.figuraKarty.K9, Karta.figuraKarty.K10, Karta.figuraKarty.KJ, Karta.figuraKarty.KD, Karta.figuraKarty.KK, Karta.figuraKarty.KA, };
+      static Karta.kolorKarty[] kolory = { Karta.kolorKarty.pik, Karta.kolorKarty.kier, Karta.kolorKarty.karo, Karta.kolorKarty.trefl };
+      //Karta[] rekaIstol = new Karta[7]; 
+      List<Karta> rekaIstol = new List<Karta>();
+
+      public void generujKarty()
+      {       
+          for (int i = 0; i < 52; i++)
+          {
+              if (i < 13)
+              {
+                  tab[i] = new Karta { figura = figury[i], kolor = kolory[0] };               
+              }
+              if (i > 12 && i<26)
+              {
+                  tab[i] = new Karta { figura = figury[i-13], kolor = kolory[1] };                 
+              }
+              if (i > 25 && i < 39)
+              {
+                  tab[i] = new Karta { figura = figury[i-26], kolor = kolory[2] };                
+              }
+              if (i > 38 && i < 52)
+              {
+                  tab[i] = new Karta { figura = figury[i-39], kolor = kolory[3] };                
+              }              
+          }
+          rekaIstol.Add(new Karta { figura = Karta.figuraKarty.K3, kolor = Karta.kolorKarty.pik });
+          rekaIstol.Add(new Karta { figura = Karta.figuraKarty.KA, kolor = Karta.kolorKarty.kier });
+          rekaIstol.Add(new Karta { figura = Karta.figuraKarty.K2, kolor = Karta.kolorKarty.pik });
+          rekaIstol.Add(new Karta { figura = Karta.figuraKarty.KK, kolor = Karta.kolorKarty.pik });
+          rekaIstol.Add(new Karta { figura = Karta.figuraKarty.KD, kolor = Karta.kolorKarty.pik });
+          rekaIstol.Add(new Karta { figura = Karta.figuraKarty.KJ, kolor = Karta.kolorKarty.pik });
+          rekaIstol.Add(new Karta { figura = Karta.figuraKarty.K10, kolor = Karta.kolorKarty.pik });
+      }
+
+      public bool szukaj(int n,Karta.kolorKarty kol)
+      {
+          for (int i = 0; i < rekaIstol.Count; i++)
+          {
+              if (((int)rekaIstol[i].figura == n) && (rekaIstol[i].kolor==kol))
+                  return true;
+          }
+              return false;
+      }
+
+      public bool szukaj(int n)
+      {
+          for (int i = 0; i < rekaIstol.Count; i++)
+          {
+              if ((int)rekaIstol[i].figura == n)
+                  return true;
+          }
+          return false;
+      }
+
+
+       public int czyPokerKrolewski()
+       {
+           if (szukaj((int)Karta.figuraKarty.K10, Karta.kolorKarty.pik) == true)
+               if (szukaj((int)Karta.figuraKarty.KJ, Karta.kolorKarty.pik) == true)
+                   if (szukaj((int)Karta.figuraKarty.KD, Karta.kolorKarty.pik) == true)
+                       if (szukaj((int)Karta.figuraKarty.KK, Karta.kolorKarty.pik) == true)
+                           if (szukaj((int)Karta.figuraKarty.KA, Karta.kolorKarty.pik) == true)
+                               return 1;
+           return 0;
+       }
+       public int czyPoker()
+       {
+
+           return 0;
+       }
+       
+    }
+}

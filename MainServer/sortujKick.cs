7e84ee3fc2816ca7d2b6a3b1679c14c0646d60c8
pawel2806 <pawelsudo2806@gmail.com>
2013-11-12 21:33:19 +0100

    mod7

---
+++ MainServer/sortujKick.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MainServer
+{
+    public class sortujKick: IComparer<Uzytkownik>
+    {
+        public int Compare(Uzytkownik a, Uzytkownik b)
+        {
+            if (a.kicker < b.kicker) return 1;
+            else if (a.kicker > b.kicker) return -1;
+            else return 0;
+        }
+    }
+}

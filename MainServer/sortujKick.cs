9ba6da60ddd21e49b011d1f0030e51c9da400992
mithrus <mithrus91@gmail.com>
2013-11-28 21:32:57 +0100

    Update sortujKick.cs
    bogdan

--- MainServer/sortujKick.cs
+++ MainServer/sortujKick.cs
@@ -1,13 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace MainServer
 {
-    public class sortujKick : IComparer<Uzytkownik>
+    public class sortujKick : IComparer<Gracz>
     {
-        public int Compare(Uzytkownik a, Uzytkownik b)
+        public int Compare(Gracz a, Gracz b)
         {
             if (a.kicker < b.kicker) return 1;
             else if (a.kicker > b.kicker) return -1;

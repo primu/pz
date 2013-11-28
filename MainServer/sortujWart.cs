c53af63a30be4f6f551fdca9351f7cef449eebc3
mithrus <mithrus91@gmail.com>
2013-11-28 21:34:20 +0100

    Update sortujWart.cs
    bogdan

--- MainServer/sortujWart.cs
+++ MainServer/sortujWart.cs
@@ -1,13 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace MainServer
 {
-    public class sortujWart : IComparer<Uzytkownik>
+    public class sortujWart : IComparer<Gracz>
     {
-        public int Compare(Uzytkownik a, Uzytkownik b)
+        public int Compare(Gracz a, Gracz b)
         {
             if (a.wart < b.wart) return 1;
             else if (a.wart > b.wart) return -1;

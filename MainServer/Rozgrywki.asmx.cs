ded26bf208e634dee599d78df242ae33cf2eb523
mithrus <mithrus91@gmail.com>
2013-12-31 00:40:57 +0100

    działa allin

--- MainServer/Rozgrywki.asmx.cs
+++ MainServer/Rozgrywki.asmx.cs
@@ -9,11 +9,13 @@ namespace MainServer
     /// <summary>
     /// Summary description for Rozgrywki
     /// </summary>
+    /// 
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
+
     public class Rozgrywki : System.Web.Services.WebService
     {
         //Tymczasowe deklaracje

5aa0d212d24a2170fff766c6831f721e8b9185ae
primu <pstroiwas@gmail.com>
2014-01-03 16:06:27 +0100

    Drobna poprawka wylogowywania

--- MainServer/Baza.cs
+++ MainServer/Baza.cs
@@ -344,7 +344,7 @@ namespace MainServer
                 id.ParameterName = "@IdPokoju";
                 tok.ParameterName = "@Token";
                 if (nowyPokoj == 0)
-                    id.Value = null;
+                    id.Value = "NULL";
                 else
                     id.Value = nowyPokoj;
                 tok.Value = token;
